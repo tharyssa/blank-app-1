de5c99f7f9bbb33d7d9eb4dfc4b4a98d5628cad9
tharyssa <tharyssa.silva@estudante.ifms.edu.br>
2025-07-14 18:19:33 +0000

    ;

--- pages/Cadastro.py
+++ pages/Cadastro.py
@@ -3,16 +3,16 @@ import streamlit as st
 st.title("Minha Página Interativa")
 st.header("Bem-vindo ao meu primeiro app!")
 
-nome = st.text_input("Qual é o seu nome?")
+nome = st.text_input("Digite o nome para cadastro?")
 idade = st.number_input("Quantos anos você tem?", min_value=1, max_value=120)
-senha = st.text_input("Digite sua senha", type="password")
+senha = st.text_input("Crie sua senha", type="password")
 
 if st.button("Enviar"):
     st.write("Olá,", nome + "!")
     st.write("Você tem", idade, "anos.")
     st.write("Sua senha foi recebida com sucesso.")
     
-tecnologia = st.checkbox("Você gosta de tecnologia?")
-nota_matematica = st.slider("Quanto você precisa se organizar", 0, 10)
+rotina = st.checkbox("Você tem alguma rotina?")
+organizacao = st.slider("Quanto você precisa se organizar?", 0, 10)
 
 st.write("Obrigado por usar meu app.")
