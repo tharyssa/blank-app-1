673d5dac6969dd38ec18870626769cebd75283bc
tharyssa <tharyssa.silva@estudante.ifms.edu.br>
2025-07-14 18:32:17 +0000

    ;

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 
-st.title("PRO") st.markdown(":blue[life]")
+st.title("PRO life")
 st.header("Folders")
 
+col1, col2, col3, col4 = st.columns(4)
+with col1:
 st.image("saude1.png" , caption="Saúde")
+with col2:
 st.image("progresso.png", caption="Progresso")
+
+with col3:
 st.image("alimentacao.png", caption="Alimentação")
+with col4:
 st.image("rotina.png", caption="Rotina")
 
 if st.button(" crie sua nova rotina"):
