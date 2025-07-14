f36d894cd1bda5d50efdd753ba6b6d9b38b9a9ec
tharyssa <tharyssa.silva@estudante.ifms.edu.br>
2025-07-14 18:33:17 +0000

    ;

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,16 @@ st.header("Folders")
 
 col1, col2, col3, col4 = st.columns(4)
 with col1:
-st.image("saude1.png" , caption="Saúde")
+    st.image("saude1.png" , caption="Saúde")
+
 with col2:
-st.image("progresso.png", caption="Progresso")
+    st.image("progresso.png", caption="Progresso")
 
 with col3:
-st.image("alimentacao.png", caption="Alimentação")
+    st.image("alimentacao.png", caption="Alimentação")
+
 with col4:
-st.image("rotina.png", caption="Rotina")
+    st.image("rotina.png", caption="Rotina")
 
 if st.button(" crie sua nova rotina"):
     st.write("Você clicou no botão!")
