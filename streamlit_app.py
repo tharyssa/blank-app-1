e98f380b9b1c4086b81d3c9c314734e0bd037bac
tharyssa <tharyssa.silva@estudante.ifms.edu.br>
2025-07-14 17:58:16 +0000

    ;

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.title("PRO life")
 st.header("Folders")
 
-st.image("/workspaces/blank-app-1/saude1.png" , caption="Saúde")
-st.image("/workspaces/blank-app-1/progresso.png", caption="Progresso")
-st.image("/workspaces/blank-app-1/alimentacao.png", caption="Alimentação")
-st.image("/workspaces/blank-app-1/rotina.png", caption="Rotina")
+st.image("saude1.png" , caption="Saúde")
+st.image("progresso.png", caption="Progresso")
+st.image("alimentacao.png", caption="Alimentação")
+st.image("rotina.png", caption="Rotina")
 
 if st.button(" crie sua nova rotina"):
     st.write("Você clicou no botão!")
