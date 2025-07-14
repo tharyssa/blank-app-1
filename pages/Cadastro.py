ffe3d7e27438f0182952c165ce4d7f469fa3e47c
tharyssa <tharyssa.silva@estudante.ifms.edu.br>
2025-07-14 18:19:25 +0000

    /

--- pages/Cadastro.py
+++ pages/Cadastro.py
@@ -5,14 +5,14 @@ st.header("Bem-vindo ao meu primeiro app!")
 
 nome = st.text_input("Qual é o seu nome?")
 idade = st.number_input("Quantos anos você tem?", min_value=1, max_value=120)
-cor = st.selectbox("Qual sua cor favorita?", ["Vermelho", "Verde", "Azul", "Amarelo"])
+senha = st.text_input("Digite sua senha", type="password")
 
 if st.button("Enviar"):
     st.write("Olá,", nome + "!")
     st.write("Você tem", idade, "anos.")
-    st.write("Sua cor favorita é:", cor)
+    st.write("Sua senha foi recebida com sucesso.")
     
 tecnologia = st.checkbox("Você gosta de tecnologia?")
-nota_matematica = st.slider("Quanto você gosta de matemática?", 0, 10)
+nota_matematica = st.slider("Quanto você precisa se organizar", 0, 10)
 
 st.write("Obrigado por usar meu app.")
